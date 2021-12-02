cda530af1dc8fb73ca8a4d589e0029718511602e
lorenzosavoia <lorenzo.savoia97@gmail.com>
2021-12-02 19:08:33 +0100

    end

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
+function getRndInteger(min, max) {
+    let bomb = [];
+    
+    for (let i = 0; i < 16; i++) {
+
+        min = Math.ceil(min);
+        max = Math.floor(max);
+
+        let element = bomb[i];
+        element = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        while (bomb.includes(element)) {
+            element = Math.floor(Math.random() * (max - min + 1)) + min;
+            
+        }
+        bomb.push(element)
+    }
+    return bomb    
+}
+
+
 let button = document.querySelector('.bottone');
+let level = document.getElementById("difficulty");
+let active = document.querySelector(".active");
+let containerGrid = document.getElementById('grid');
+
 button.addEventListener('click', function () {
-    let level = document.getElementById("difficulty");
-    let active = document.querySelector(".active");
-    let containerGrid = document.getElementById('grid');
+
+    const bombLvl1 = getRndInteger(1, 100);
+    const bombLvl2 = getRndInteger(1, 81);
+    const bombLvl3 = getRndInteger(1, 49);
 
 
     if (level.value == '1') {
@@ -21,6 +47,23 @@ button.addEventListener('click', function () {
             square.style.height = `calc(100% / ${row})`
             square.append([i + 1])
             containerGrid.append(square);
+
+            square.addEventListener("click", function() {
+                if (bombLvl1.includes(i + 1)) {
+                 
+                for (let y = 0; y < numberSquare; y++) {
+                    const red = document.querySelectorAll(".square")
+                    if (bombLvl1.includes(y + 1)) {
+                        red[y].classList.add("bomb");
+                        containerGrid.replaceChild(red[y].cloneNode(true), red [y]);
+                    }    
+                    
+                } 
+
+                }else {
+                    this.classList.add("color-blue");
+                }
+            })
 
         }
 
@@ -41,6 +84,24 @@ button.addEventListener('click', function () {
             square.append([i + 1])
             containerGrid.append(square);
 
+            square.addEventListener("click", function () {
+                if (bombLvl1.includes(i + 1)) {
+
+                    for (let y = 0; y < numberSquare; y++) {
+                        const red = document.querySelectorAll(".square")
+                        if (bombLvl1.includes(y + 1)) {
+                            red[y].classList.add("bomb");
+                            containerGrid.replaceChild(red[y].cloneNode(true), red[y]);
+                        }
+
+                    }
+
+                } else {
+                    this.classList.add("color-blue");
+                }
+            })
+
+
         }
     } else {
         containerGrid.innerHTML = "";
@@ -57,6 +118,24 @@ button.addEventListener('click', function () {
             square.style.height = `calc(100% / ${row})`
             square.append([i + 1])
             containerGrid.append(square);
+
+            square.addEventListener("click", function () {
+                if (bombLvl1.includes(i + 1)) {
+
+                    for (let y = 0; y < numberSquare; y++) {
+                        const red = document.querySelectorAll(".square")
+                        if (bombLvl1.includes(y + 1)) {
+                            red[y].classList.add("bomb");
+                            containerGrid.replaceChild(red[y].cloneNode(true), red[y]);
+                        }
+
+                    }
+
+                } else {
+                    this.classList.add("color-blue");
+                }
+            })
+
 
         }
     }
